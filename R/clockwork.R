f2bf09aa203c9a2091597473f565c4213a26f1bb
daranzolin <daranzolin@gmail.com>
2019-09-29 14:36:42 -0700

    Initial commit

---
+++ R/clockwork.R
@@ -0,0 +1,125 @@
+#' Create a clockwork widget
+#'
+#' @param data a tabular object
+#' @param x a numeric variable
+#' @param y a numeric variable
+#' @param cycle a variable to cycle/overlay subsequent lines
+#' @param x_ticks number of ticks for x-axis
+#' @param y_ticks number of ticks for y-axis
+#' @param cycle_label cycle label
+#' @param width_height widget width and height
+#' @param loop_interval cycle length in milliseconds
+#' @param repeat_cycles whether to repeat visualization after all cycles
+#'
+#' @import htmlwidgets
+#'
+#' @export
+clockwork <- function(data,
+                      x,
+                      y,
+                      cycle,
+                      x_ticks = 4,
+                      y_ticks = 3,
+                      show_cycle_stats = FALSE,
+                      cycle_label = "Cycle: ",
+                      width_height = 400,
+                      loop_interval = 4000,
+                      repeat_cycles = TRUE
+                      ) {
+
+  x <- rlang::enquo(x)
+  y <- rlang::enquo(y)
+  cycle <- rlang::enquo(cycle)
+
+  data_df <- as.data.frame(data)
+  out_df <- data.frame(x = rep(NA, nrow(data_df)), stringsAsFactors = FALSE)
+  out_df$xValue <- data_df[,quo_name(x)]
+  if (!is.numeric(out_df$xValue)) stop("x must be numeric", call. = FALSE)
+  out_df$yValue <- data_df[,quo_name(y)]
+  if (!is.numeric(out_df$yValue)) stop("y must be numeric", call. = FALSE)
+  out_df$cycle <- data_df[,quo_name(cycle)]
+  cycles <- unique(out_df$cycle)
+
+  x = list(
+    data = out_df,
+    cycles = cycles,
+    show_cycle_stats = show_cycle_stats,
+    x_ticks = x_ticks,
+    y_ticks = y_ticks,
+    cycle_label = cycle_label,
+    width_height = width_height,
+    repeat_cycles = repeat_cycles,
+    loop_interval = loop_interval
+  )
+
+  htmlwidgets::createWidget(
+    name = 'clockwork',
+    x,
+    package = 'clockwork'
+  )
+}
+
+#' Style a clockwork widget
+#'
+#' @param clockwork a clockwork object
+#' @param active_line_color color of active line being drawn
+#' @param past_line_color color of past lines drawn
+#' @param past_line_opacity opacity of past lines drawn
+#' @param grid_line_color color of grid lines
+#' @param x_axis_font_size x-axis label font-size
+#' @param y_axis_font_size y-axis label font-size
+#' @param inner_stats_font_size inner stats font size
+#' @param font_family font family
+#'
+#' @return a clockwork widget
+#' @export
+#'
+#' @examples
+cw_style <- function(clockwork,
+                     active_line_color = "#da4f81",
+                     past_line_color = "#ccc",
+                     past_line_opacity = 0.3,
+                     grid_line_color = "#000",
+                     x_axis_font_size = 10,
+                     y_axis_font_size = 10,
+                     inner_stats_font_size = 12,
+                     font_family = "Arial") {
+
+  clockwork$x$active_line_color = active_line_color
+  clockwork$x$past_line_color = past_line_color
+  clockwork$x$grid_line_color = grid_line_color
+  clockwork$x$past_line_opacity <- past_line_opacity
+  clockwork$x$x_axis_font_size <- x_axis_font_size
+  clockwork$x$y_axis_font_size <- y_axis_font_size
+  clockwork$x$inner_stats_font_size <- inner_stats_font_size
+  clockwork$x$font_family <- font_family
+  return(clockwork)
+}
+
+#' Shiny bindings for clockwork
+#'
+#' Output and render functions for using clockwork within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a clockwork
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name clockwork-shiny
+#'
+#' @export
+clockworkOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'clockwork', width, height, package = 'clockwork')
+}
+
+#' @rdname clockwork-shiny
+#' @export
+renderClockwork <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, clockworkOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/clockwork.js
@@ -0,0 +1,225 @@
+HTMLWidgets.widget({
+
+  name: 'clockwork',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(opts) {
+
+        let data = HTMLWidgets.dataframeToD3(opts.data);
+        //console.log(data);
+        let currentMin;
+        let currentMax;
+        let currentMean;
+        let minMaxData;
+        let radialMinMaxData;
+        let width = opts.width_height;
+        let height = opts.width_height;
+        let cycle_label = opts.cycle_label;
+        let textData = opts.show_cycle_stats ?  [cycle_label, "min: ", "max: ", "mean: "] : [cycle_label];
+        let ticks = opts.x_ticks;
+        let margin = 10;
+        let innerRadius = width / 5;
+        let outerRadius = (width / 2) - margin;
+        let loopInterval = opts.loop_interval;
+        let data_x_extent = d3.extent(data, d => d.xValue);
+        let x_steps = Math.round(data_x_extent[1]/ticks);
+        let tick_marks = d3.range(data_x_extent[0], data_x_extent[1], x_steps);
+        let textClasses = ["currentCycle", "currentMin", "currentMax", "currentMean"];
+        let currentCycle = opts.cycles[0];
+        let n_cycles = opts.cycles.length;
+        let i = 0;
+        let active_line_color = opts.hasOwnProperty("active_line_color") ? opts.active_line_color : "#da4f81";
+        let past_line_color = opts.hasOwnProperty("past_line_color") ? opts.past_line_color : "#ccc";
+        let past_line_opacity = opts.hasOwnProperty("past_line_opacity") ? opts.past_line_opacity : 0.3;
+        let grid_line_color = opts.hasOwnProperty("grid_line_color") ? opts.grid_line_color : "#000";
+        // let background_fill = opts.hasOwnProperty("background_fill") ? opts.background_fill : "white";
+        let x_axis_font_size = opts.hasOwnProperty("x_axis_font_size") ? opts.x_axis_font_size : 10;
+        let y_axis_font_size = opts.hasOwnProperty("y_axis_font_size") ? opts.y_axis_font_size : 10;
+        let inner_stats_font_size = opts.hasOwnProperty("inner_stats_font_size") ? opts.inner_stats_font_size : 12;
+        let font_family = opts.hasOwnProperty("font_family") ? opts.font_family : "Arial";
+        let stop_cycles;
+
+        const svg = d3.select(el)
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .attr("viewBox", [-width / 2, -height / 2, width, height]);
+          // .attr("viewport-fill", backgr);
+          //.attr("preserveAspectRatio", "xMaxYMax meet");
+
+      /*svg.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("fill", background_fill); */
+
+      let xScale = d3.scaleLinear()
+          .domain(d3.extent(data, d => d.xValue))
+          .range([0, 2 * Math.PI]);
+
+      let xAxis = g => g
+          .call(g => g.selectAll("g")
+            .data(tick_marks)
+            .enter().append("g")
+              .call(g => g.append("path")
+                  .attr("class", "spokes")
+                  .attr("stroke", grid_line_color)
+                  .attr("stroke-opacity", 0.2)
+                  .attr("d", d => `
+                    M${d3.pointRadial(xScale(d), innerRadius)}
+                    L${d3.pointRadial(xScale(d), outerRadius)}
+                  `)));
+
+        let xAxisLabels = tick_marks.map(d => {
+              let p = d3.pointRadial(xScale(d), innerRadius - 7);
+              return {label: d,
+                      x: p[0],
+                      y: p[1]};
+            });
+
+        let yScale = d3.scaleLinear()
+          .domain(d3.extent(data, d => d.yValue))
+          .range([innerRadius, outerRadius]);
+
+        let yAxis = g => g
+          //.attr("text-anchor", "middle")
+          //.attr("font-family", "sans-serif")
+          .attr("font-size", `${y_axis_font_size}px`)
+          .attr("font-family", font_family)
+          .call(g => g.selectAll("g")
+            .data(yScale.ticks(opts.y_ticks).reverse())
+            .enter().append("g")
+              .attr("fill", "none")
+              .call(g => g.append("circle")
+                  .attr("stroke", grid_line_color)
+                  .attr("stroke-opacity", 0.2)
+                  .attr("r", d => yScale(d)))
+              .call(g => g.append("text")
+                  .attr("y", d => -yScale(d))
+                  .attr("stroke", "#fff")
+                  .attr("font-size", `${y_axis_font_size}px`)
+                  .attr("stroke-width", 5)
+                  .text((d, i) => `${d.toFixed(0)}`)
+                .clone(false)
+                  .attr("y", d => yScale(d))
+                .selectAll(function() { return [this, this.previousSibling]; })
+                .clone(true)
+                  .attr("fill", "currentColor")
+                  .attr("stroke", "none")));
+
+        let legend = g => g.append("g")
+          .selectAll("g")
+          .data(textData)
+          .enter().append("g")
+            .attr("transform", (d, i) => `translate(-60,${(i - (textData.length - 1) / 2) * 20})`)
+            .call(g => g.append("text")
+                .style("text-anchor", "left")
+                .style("font-size", `${inner_stats_font_size}px`)
+                .attr("font-family", font_family)
+                .attr("class", (d, i) => textClasses[i])
+                .attr("x", 50)
+                .attr("y", 0)
+                .attr("dy", "0.35em")
+                .text(d => d));
+
+        let line = d3.lineRadial()
+            .defined(d => d.yValue !== "NA")
+            .curve(d3.curveLinearClosed)
+            .angle(d => xScale(d.xValue));
+
+        svg.append("g")
+          .call(xAxis);
+
+        svg.append("g")
+          .call(yAxis);
+
+        svg.append("g")
+          .call(legend);
+
+        svg.selectAll(".xAxisText")
+          .data(xAxisLabels)
+          .enter().append("text")
+          .attr("x", d => d.x)
+          .attr("y", d => d.y)
+          .attr("text-anchor", "middle")
+          .attr("font-size", `${x_axis_font_size}px`)
+          .attr("font-family", font_family)
+          .text(d => d.label);
+
+          console.log(opts.repeat_cycles);
+
+        let ticker = d3.interval(e => {
+
+          currentCycle = opts.cycles[i];
+
+          if (i === n_cycles) {
+            if (!opts.repeat_cycles) {
+              ticker.stop();
+              stop_cycles = true;
+              currentCycle = "";
+            }
+            else {
+              d3.selectAll(".lineclass").remove();
+              i = 0;
+              currentCycle = opts.cycles[i];
+            }
+          }
+
+            d3.select(".currentCycle").text(d => d + currentCycle);
+
+            let filtered_data = data.filter(d => d.cycle === currentCycle);
+            let path = svg.append("path")
+              .attr("class", "lineclass")
+              .attr("fill", "none")
+              .attr("stroke", active_line_color)
+              .attr("stroke-width", 1.5)
+              .attr("d", line.radius(d => yScale(d.yValue))(filtered_data));
+
+             let totalLength = path.node().getTotalLength();
+                  path
+                  .attr("stroke-dasharray", totalLength + " " + totalLength)
+                  .attr("stroke-dashoffset", totalLength)
+                  .transition()
+                  .duration(loopInterval)
+                  .ease(d3.easeLinear)
+                  .attr("stroke-dashoffset", 0)
+                  .on("end", function() {
+                    d3.select(this)
+                      .attr("opacity", past_line_opacity)
+                      .attr("stroke", past_line_color);
+                  });
+
+            let minmax = d3.extent(filtered_data, d => d.yValue);
+            currentMin = stop_cycles ? "" : minmax[0];
+            currentMax = stop_cycles ? "" : minmax[1];
+            currentMean = stop_cycles ? "" : Math.round(d3.mean(filtered_data, d => d.yValue));
+            d3.select(".currentMin").text(d => d + currentMin);
+            d3.select(".currentMax").text(d => d + currentMax);
+            d3.select(".currentMean").text(d => d + currentMean);
+
+            minMaxData = filtered_data.filter(d => d.yValue === currentMin || d.yValue === currentMax);
+            radialMinMaxData = minMaxData.map(d => d3.pointRadial(xScale(d.xValue), innerRadius));
+            i = i + 1;
+            currentCycle = opts.cycles[i];
+
+    }, loopInterval);
+
+    },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
